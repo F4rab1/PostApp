c95bf44aa7d90257090e5879f4ce0bfc1b87c56d
F4rab1 <issafarabi2022@gmail.com>
2023-01-02 17:34:48 +0600

    Initial Commit

---
+++ PostApp/PostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostAppApp.swift
+//  PostApp
+//
+//  Created by Фараби Иса on 02.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
